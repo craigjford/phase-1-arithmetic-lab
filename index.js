baada07268b4c2cd455aa0af3c329b9247c90ebf
Craig Ford <craig_ford@icloud.com>
2022-01-31 19:52:52 -0500

    commit tree 9c909efa9c1be45dba0ff378599fdd21df00d638

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.round(Math.random() * 10) + 1;
+
+let num3 = 9;
+let num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(2, 4, 6, 8, 12, 16, 20);
